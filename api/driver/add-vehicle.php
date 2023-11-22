e95302528b1f4c46bd747326a753fc377f778693
Prenero Solutions <prenerostudio@gmail.com>
2023-11-22 15:04:53 +0500

    20

--- api/driver/add-vehicle.php
+++ api/driver/add-vehicle.php
@@ -7,41 +7,22 @@ header('Cache-Control: max-age=3600');
 
 include("../../config.php");
 
-// Function to handle image upload
-function uploadImage() {
-    $targetDir = "img/vehicles/";
-    $targetFilePath = $targetDir . basename($_FILES["v_img"]["name"]);
-    $fileType = strtolower(pathinfo($targetFilePath, PATHINFO_EXTENSION));
-    $allowTypes = array('jpg', 'png', 'jpeg', 'gif');
 
-    if (in_array($fileType, $allowTypes)) {
-        if (move_uploaded_file($_FILES["v_img"]["tmp_name"], $targetFilePath)) {
-            return $targetFilePath;
-        } else {
-            return false;
-        }
-    }
-    return false;
-}
 $d_id  = $_POST['d_id'];
 $v_id = $_POST['v_id'];
 $date = date("Y-m-d H:i:s");
 
-// Handle image upload
-    $v_img = uploadImage();
 
-
-
-if(isset($_POST['vname'])){ 	 	        		
+if(isset($_POST['v_id'])){ 	 
+	
 		$sql="INSERT INTO `driver_vehicle`(
-													`v_id`, 
-													`d_id`, 
-													`date_v_add`
-													) VALUES (
-													'[value-1]',
-													'[value-2]',
-													'[value-3]',
-													'[value-4]')";				
+											`v_id`, 
+											`d_id`, 
+											`date_v_add`
+											) VALUES (
+											'$v_id',
+											'$d_id',
+											'$date')";				
 		
 		$r=mysqli_query($connect,$sql);
 		if($r){    
