fbef628c1639a4626de0bdb45c1027e0e6a43306
Atiq Ramzan <prenerosolutions@gmail.com>
2023-12-22 00:41:39 +0500

    22

--- dashboard.php
+++ dashboard.php
@@ -20,17 +20,16 @@ include('header.php');
                 <div class="btn-list">
                   <span class="d-none d-sm-inline">
                     <a href="#" class="btn">
-                      New view
+						 <i class="ti ti-user-search" style="margin-right: 10px;"></i>
+                     Driver Tracking
                     </a>
                   </span>
                   <a href="#" class="btn btn-primary d-none d-sm-inline-block" data-bs-toggle="modal" data-bs-target="#modal-report">
-                    <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
-                    Create new report
+                   <i class="ti ti-bookmark-plus" style="margin-right: 10px;"></i>
+                    Create New Booking
                   </a>
                   <a href="#" class="btn btn-primary d-sm-none btn-icon" data-bs-toggle="modal" data-bs-target="#modal-report" aria-label="Create new report">
-                    <!-- Download SVG icon from http://tabler-icons.io/i/plus -->
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" stroke-linecap="round" stroke-linejoin="round"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M12 5l0 14" /><path d="M5 12l14 0" /></svg>
+                    <i class="ti ti-bookmark-plus"></i>
                   </a>
                 </div>
               </div>
