b6415aa1b7febd3b15dbe7efe63ea0ba73fc5c04
Prenero Solutions <prenerostudio@gmail.com>
2024-04-12 16:37:07 +0500

    12

--- view-booker.php
+++ view-booker.php
@@ -54,7 +54,13 @@ $brow = mysqli_fetch_array($bsql);
 								<i class="ti ti-calendar-user"></i>
 								Booker Bookings Statement
 							</a>                      												
-						</li>										
+						</li>	
+						<li class="nav-item">												
+							<a href="#tabs-wallet" class="nav-link" data-bs-toggle="tab">
+								<i class="ti ti-calendar-user"></i>
+								Booker Wallet
+							</a>                      												
+						</li>	
 					</ul>                  								
 				</div>				
 				<div class="card-body">                								
@@ -319,7 +325,123 @@ $brow = mysqli_fetch_array($bsql);
 									</div>																														
 								</div>
 							</div>                      									
-						</div>                   				
+						</div> 
+						<div class="tab-pane" id="tabs-wallet">						
+							<div class="card-body">							
+								<h2 class="mb-4">Booker Wallet</h2>								
+								<div class="row mb-3">								
+									<div class="card"> 
+										<div class="card-body border-bottom py-3">
+											<div id="table-adriver" class="table-responsive">	
+												<table class="table">
+													<thead>													
+														<tr>														
+															<th>															
+																<button class="table-sort" data-sort="sort-id">ID</button>
+															</th>															
+															<th>					
+																<button class="table-sort" data-sort="sort-date">Job Completion Date</button>
+															</th>															
+															<th>					
+																<button class="table-sort" data-sort="sort-time">Job Details</button>
+															</th>
+															 
+															<th>													
+																<button class="table-sort" data-sort="sort-passenger">Total Comission</button>
+															</th> 												
+															<th>													
+																<button class="table-sort" data-sort="sort-passenger">Status</button>
+															</th>															
+															<th>															
+																<button class="table-sort">Actions</button>															
+															</th>														
+														</tr>													
+													</thead>
+													<tbody class="table-tbody">													
+														<?php																									
+															 $x = 0;																 
+															   $isql = mysqli_query($connect, "SELECT ba.*, c.*, bk.*, bt.* FROM booker_account AS ba JOIN clients AS c ON ba.c_id = c.c_id JOIN bookings AS bk ON ba.book_id = bk.book_id JOIN booking_type AS bt ON bk.b_type_id = bt.b_type_id WHERE ba.c_id = '$c_id'");
+															   while ($irow = mysqli_fetch_array($isql)) :
+															   $x++;														
+														?>
+														<tr>														
+															<td class="sort-id">															
+																<?php echo $x; ?>															
+															</td>																								
+															<td class="sort-time">															
+																<?php echo $irow['commission_date']; ?>															
+															</td>
+															<td class="sort-passenger">															
+																Booking ID: <?php echo $irow['book_id']; ?> <br>
+																<?php echo $irow['pickup']; ?> - <?php echo $irow['destination']; ?>
+															</td>															
+															
+															<td class="sort-pickup">															
+																<?php echo $irow['comission_amount']; ?>															
+															</td>
+															<td class="sort-pickup">
+																
+																<?php 											
+									
+																if($irow['comission_status']=='Unpaid'){
+										
+																?>												
+									
+																<div class="col-auto status">
+										
+																	<span class="status-dot status-dot-animated bg-red d-block"></span>															
+											
+																	<span>Unpaid</span>									
+										
+																</div>
+										
+																<?php											
+								
+																} else{											
+										
+																?>
+										
+																<div class="col-auto status">
+											
+																	<span class="status-dot status-dot-animated bg-green d-block"></span>											
+											
+																	<span>Paid</span>											
+										
+																</div>			
+										
+																<?php
+									
+																}
+										
+																?>				
+																
+															
+															</td>					
+															<td>																			
+																<a href="booker-invoice.php?acc_id=<?php echo $irow['acc_id']; ?>">
+																	<button class="btn btn-info">																	
+																		<i class="ti ti-eye"></i>
+																		View Invoice																	
+																	</button>
+																</a>																
+															</td>														
+														</tr>                          																				
+														<?php endwhile; ?>
+														<?php if ($x === 0) : ?>														
+														<tr>
+															<td colspan="8">																						
+																<p align="center">No Invoice Found!</p>	
+															</td>
+														</tr>
+														<?php  endif; ?>
+													</tbody>				
+												</table>												
+											</div>					
+										</div>																						
+									</div>																														
+								</div>
+							</div>                      									
+						</div>
 					</div>                 										
 				</div>             
 			</div>
