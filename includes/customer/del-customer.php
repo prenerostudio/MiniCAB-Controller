45083f0ee534f4f14de3c2ea7624bf9b6f81c703
Prenero Studio <prenerostudio@gmail.com>
2025-09-26 15:26:31 +0500

    25-09

--- includes/customer/del-customer.php
+++ includes/customer/del-customer.php
@@ -1,20 +1,14 @@
 <?php
-include('config.php');
-include('session.php');
+include('../../config.php');
+include('../../session.php');
 	
 $c_id = $_GET['c_id'];
 $sql = "DELETE FROM `clients` WHERE `c_id`='$c_id'";
 $result = $connect->query($sql);
-
 if($result){ 	
-
     $activity_type = 'Customer Profile Deleted';	
-
     $user_type = 'user';	
-
-    $details = "Customer Profile Has Been Deleted by Controller.";
-	
-
+    $details = "Customer Profile Has Been Deleted by Controller.";	
     $actsql = "INSERT INTO `activity_log`(
 					`activity_type`, 
 					`user_type`, 
@@ -24,14 +18,10 @@ if($result){
 					'$activity_type',
 					'$user_type',
 					'$myId',
-					'$details')";
-	
-
+					'$details')";	
     $actr = mysqli_query($connect, $actsql);
-
-    header('location: customers.php');
+    header('location: ../../customers.php');
 } else {
-
-    header('location: customers.php');
+    header('location: ../../customers.php');
 }
 ?>
